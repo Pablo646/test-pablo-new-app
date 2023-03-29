1bafd2e2d7631d822edbe67832b7b8f3d93cffbf
Pablo646 <pablow.g64@gmail.com>
2023-03-29 11:49:53 +0200

    Challenge3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,29 @@
 function PokemonCard(){
-    return <figure><img
+  const pokemon = pokemonList[0]     
+  return (    <figure className="PokemonCard">
+     {pokemon.imgSrc ? <img className="PokemonCardImg" src={pokemon.imgSrc} alt={pokemon.name}/> : <p>"???"</p>}
+      <figcaption className="BulbasaurCase">{pokemon.name}</figcaption>    </figure>)    }
+  //  const pokemon = pokemonList[0]
+  //  return(
+  //   <figure className="PokemonCard"/> ({ pokemon.imgSrc?<img src={pokemon.imgSrc} alt={pokemon.name}/> :<p>"???"</p> }
+  //  ))
+    /*return <figure className="PokemonCard"><img className="PokemonCardImg"
     src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
     alt="bulbasaur"
-  /> <figcaption>Bulbasaur</figcaption>
-  </figure>
-}
+  /> <figcaption className="BulbasaurCase">Bulbasaur</figcaption>
+  </figure>*/
+
+
+
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default PokemonCard;
