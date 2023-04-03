6fedb6e1c8b388c18794dbcc175b5547436d33dd
Pablo646 <pablow.g64@gmail.com>
2023-04-03 09:38:35 +0200

    State.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,53 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 function App() {
+ const[pokemonIndex , pokemonFutur]=useState(0)
+const incrementation=()=>{pokemonFutur(pokemonIndex+1)}
+const decrementation=()=>{pokemonFutur(pokemonIndex-1)}
+
   return (
-    <div>
-   <PokemonCard pokemon={pokemonList[0]}/>
+    <div className='masterDiv'>
+   <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+   <div className='btnContainer'>    
+   {pokemonIndex > 0 ? <button className='btnIndexSelector' onClick={decrementation}>Precédent</button>:null}
+   {pokemonIndex < pokemonList.length -1 ?<button className='btnIndexSelector' onClick={incrementation}>Suivant</button>:null}
+   </div>
     </div>
+
   );
 }
 
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,16 +4,19 @@ import PropTypes from "prop-types"
 function PokemonCard({pokemon}){  
   return (    <figure className="PokemonCard">
      {pokemon.imgSrc ? <img className="PokemonCardImg" src={pokemon.imgSrc} alt={pokemon.name}/> : <p>"???"</p>}
-      <figcaption className="BulbasaurCase">{pokemon.name}</figcaption>    </figure>) 
+      <figcaption className="BulbasaurCase">{pokemon.name}</figcaption>    </figure>
+      ) 
       }
       
-  
-  PokemonCard.propTypes = {
-    pokemon: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      imgSrc: PropTypes.string,
-    }).isRequired}
-
-
-
-export default PokemonCard
+      
+      PokemonCard.propTypes = {
+        pokemon: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          imgSrc: PropTypes.string,
+        }).isRequired}
+        
+        
+        
+        export default PokemonCard
+        
+        
